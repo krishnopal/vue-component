644844ecec75dc51881a2dc0d4d78f63355c3892
shuvo pal <spshuvo850@gmail.com>
2021-03-20 10:32:51 +0600

    component is updated

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,24 @@
 <template>
-    <div class=" w-full flex " >
-        <!-- header -->
-        <section><Header/></section>
-        <!-- Mainbar -->
 
-    <section>
-        <div class="w-full mt-16  px-3">
-        <div class=" px-4 flex flex-wrap bg-gray-400 py-5">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore reiciendis asperiores iste laudantium ipsa praesentium aliquid laborum error voluptas sint, rem sit possimus minus numquam distinctio illo dicta impedit ipsum?</p>
+    <div class="flex flex-wrap overflow-hidden">
+        <div class="w-full overflow-hidden">
+            <Header/>
         </div>
+        <div class="w-full overflow-hidden">
+            <section>
+                <div class="w-full mt-16  px-3">
+                    <div class=" px-4 flex flex-wrap bg-gray-400 py-5">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore reiciendis asperiores iste laudantium ipsa praesentium aliquid laborum error voluptas sint, rem sit possimus minus numquam distinctio illo dicta impedit ipsum?</p>
+                    </div>
+                </div>
+            </section>
         </div>
-    </section>
-
-        <!-- Footer -->
-        <!-- <section>
-           <Footer/>
-        </section> -->
+        <div class="w-full overflow-hidden">
+            <Footer/>
+        </div>
     </div>
+
+
 </template>
 
 <script setup>
--- src/components/inc/Footer.vue
+++ src/components/inc/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="py-2 mt-3 bg-blue-600 fixed text-center font-blod flex w-full h-full">
+    <div class="py-2 mt-3 bg-blue-600 fixed text-center font-blod flex w-full h-16">
             <div class="text-white">This is Footer Vue Components</div>
         </div>
 </template>

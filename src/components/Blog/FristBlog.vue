de901bdeb08fda7c0418dd677a495de9d61b6e8a
shuvo pal <spshuvo850@gmail.com>
2021-03-25 18:30:28 +0600

    Blog Component Addad

---
+++ src/components/Blog/FristBlog.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="flex flex-wrap -mx-1 overflow-hidden sm:-mx-1 md:-mx-1 lg:-mx-1 xl:-mx-1">
+        <div class="my-1 px-1 w-full overflow-hidden sm:my-1 sm:px-1 md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/4 xl:my-1 xl:px-1 xl:w-1/4">
+            <div class="w-full pb-8">
+                <div class="mx-4 mt-10 bg-gray-300 wow fadeIn border-2 border-gary-700 shadow-xl hover:shadow-none transition duration-500 ease-in-out" data-wow-duration="1s" data-wow-delay="0.2s">
+                    <div class="mb-3 h-40 w-full bg-blue-400">
+                        <img class="w-full" src="/src/assets/blog-1.jpg" alt="blog">
+                    </div>
+                    <div class="px-3 pb-3">
+                        <ul class="flex mb-2 text-sm">
+                            <li class="w-1/2">Posted By: <a class="text-blue-500 hover:underline ml-1" href="">Admin</a></li>
+                            <li class="w-1/2 text-right">03 June, 2023</li>
+                        </ul>
+                        <p class="mb-2 text-sm  text-gray-900 text-justify">Lorem ipsuamet conset sadips cing elitr seddiam nonu eirmod tempor invidunt labore.Lorem ipsuamet conset sadips cing elitr seddiam nonu eirmod tempor invidunt labore..</p>
+                        <a class="hover:text-gray-600" href="">
+                            Learn More
+                            <i class="ml-2 text-sm lni lni-chevron-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="my-1 px-1 w-full overflow-hidden sm:my-1 sm:px-1 md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/4 xl:my-1 xl:px-1 xl:w-1/4">
+            <div class="w-full pb-8">
+                <div class="mx-4 mt-10 bg-gray-300 wow fadeIn border-2 border-gary-700 shadow-xl hover:shadow-none transition duration-500 ease-in-out" data-wow-duration="1s" data-wow-delay="0.2s">
+                    <div class="mb-3 h-40 w-full bg-blue-400">
+                        <img class="w-full" src="/src/assets/blog-2.jpg" alt="blog">
+                    </div>
+                    <div class="px-3 pb-3">
+                        <ul class="flex mb-2 text-sm">
+                            <li class="w-1/2">Posted By: <a class="text-blue-500 hover:underline ml-1" href="">Admin</a></li>
+                            <li class="w-1/2 text-right">03 June, 2023</li>
+                        </ul>
+                        <p class="mb-2 text-sm  text-gray-900 text-justify">Lorem ipsuamet conset sadips cing elitr seddiam nonu eirmod tempor invidunt labore.Lorem ipsuamet conset sadips cing elitr seddiam nonu eirmod tempor invidunt labore..</p>
+                        <a class="hover:text-gray-600" href="">
+                            Learn More
+                            <i class="ml-2 text-sm lni lni-chevron-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="my-1 px-1 w-full overflow-hidden sm:my-1 sm:px-1 md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/4 xl:my-1 xl:px-1 xl:w-1/4">
+            <div class="w-full pb-8">
+                <div class="mx-4 mt-10 bg-gray-300 wow fadeIn border-2 border-gary-700 shadow-xl hover:shadow-none transition duration-500 ease-in-out" data-wow-duration="1s" data-wow-delay="0.2s">
+                    <div class="mb-3 h-40 w-full bg-blue-400">
+                        <img class="w-full" src="/src/assets/blog-3.jpg" alt="blog">
+                    </div>
+                    <div class="px-3 pb-3">
+                        <ul class="flex mb-2 text-sm">
+                            <li class="w-1/2">Posted By: <a class="text-blue-500 hover:underline ml-1" href="">Admin</a></li>
+                            <li class="w-1/2 text-right">03 June, 2023</li>
+                        </ul>
+                        <p class="mb-2 text-sm  text-gray-900 text-justify">Lorem ipsuamet conset sadips cing elitr seddiam nonu eirmod tempor invidunt labore.Lorem ipsuamet conset sadips cing elitr seddiam nonu eirmod tempor invidunt labore..</p>
+                        <a class="hover:text-gray-600" href="">
+                            Learn More
+                            <i class="ml-2 text-sm lni lni-chevron-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="my-1 px-1 w-full overflow-hidden sm:my-1 sm:px-1 md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/4 xl:my-1 xl:px-1 xl:w-1/4">
+            <div class="w-full pb-8">
+                <div class="mx-4 mt-10 bg-gray-300 wow fadeIn border-2 border-gary-700 shadow-xl hover:shadow-none transition duration-500 ease-in-out" data-wow-duration="1s" data-wow-delay="0.2s">
+                    <div class="mb-3 h-40 w-full bg-blue-400">
+                        <img class="w-full" src="/src/assets/blog-3.jpg" alt="blog">
+                    </div>
+                    <div class="px-3 pb-3">
+                        <ul class="flex mb-2 text-sm">
+                            <li class="w-1/2">Posted By: <a class="text-blue-500 hover:underline ml-1" href="">Admin</a></li>
+                            <li class="w-1/2 text-right">03 June, 2023</li>
+                        </ul>
+                        <p class="mb-2 text-sm  text-gray-900 text-justify">Lorem ipsuamet conset sadips cing elitr seddiam nonu eirmod tempor invidunt labore.Lorem ipsuamet conset sadips cing elitr seddiam nonu eirmod tempor invidunt labore..</p>
+                        <a class="hover:text-gray-600" href="">
+                            Learn More
+                            <i class="ml-2 text-sm lni lni-chevron-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -175,6 +175,22 @@
                     </div>
                     <!-- From Component End-->
 
+                    <!-- Blog Component -->
+                    <div class="hover:bg-gray-500 hover:text-white shadow-md transition duration-300 ease-in-out rounded-lg bg-white text-xl text-green-800 font-bold py-2 w-full px-3 border-2 border-green-600 cursor-pointer my-3">
+                        <p>Blog Component</p>
+                    </div>
+                    <div class="bg-white border-2 border-black p-5">
+                        <div class="mb-4">
+                            <FristBlog/>
+                        </div>
+                    </div>
+                    <!-- Blog Component End-->
+
+                    
+
+
+                    
+
                 </div>
             </section>
         </div>
@@ -225,6 +241,8 @@ import HomeView from '../components/HomeView/HomeView.vue'
 
 import FristFrom from '../components/From/FristFrom.vue'
 import SecondFrom from '../components/From/SecondFrom.vue'
+
+import FristBlog from '../components/Blog/FristBlog.vue'
 
 
 //  v-if="showButton"
